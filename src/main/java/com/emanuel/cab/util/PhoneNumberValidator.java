73373429f7938baa929d8e75533cf4bdd10014fd
emibotea1 <Emanuel.Botea@rinf.tech>
2023-01-26 10:37:28 +0200

    created validator classes for phone number

--- src/main/java/com/emanuel/cab/util/PhoneNumberValidator.java
+++ src/main/java/com/emanuel/cab/util/PhoneNumberValidator.java
@@ -1,4 +1,28 @@
 package com.emanuel.cab.util;
 
-public class PhoneNumberValidator {
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneNumberValidator implements ConstraintValidator<ValidPhoneNumber, String> {
+
+    private static final String PHONE_PATTERN = "/^(?:(?:(?:00\\s?|\\+)40\\s?|0)(?:7\\d{2}\\s?\\d{3}\\s?\\d{3}|(21|31)\\d{1}\\s?\\d{3}\\s?\\d{3}|((2|3)[3-7]\\d{1})\\s?\\d{3}\\s?\\d{3}|(8|9)0\\d{1}\\s?\\d{3}\\s?\\d{3}))$/";
+
+    @Override
+    public void initialize(ValidPhoneNumber constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
+        return validatePhoneNumber(phoneNumber);
+    }
+
+    private boolean validatePhoneNumber(String phoneNumber) {
+        Pattern pattern = Pattern.compile(PHONE_PATTERN);
+        Matcher matcher = pattern.matcher(phoneNumber);
+
+        return matcher.matches();
+    }
 }
--- src/main/java/com/emanuel/cab/util/ValidPhoneNumber.java
+++ src/main/java/com/emanuel/cab/util/ValidPhoneNumber.java
@@ -1,4 +1,19 @@
 package com.emanuel.cab.util;
 
-public interface ValidPhoneNumber {
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE, FIELD, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = PhoneNumberValidator.class)
+@Documented
+public @interface ValidPhoneNumber {
+    String message() default "Invalid phone number";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
