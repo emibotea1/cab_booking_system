5a2c8f095d302d0f9436dbaeaa084483758a6c7e
emibotea1 <Emanuel.Botea@rinf.tech>
2023-01-31 12:38:09 +0200

    formated the code and added model and view

--- src/main/java/com/emanuel/cab/controller/HomeController.java
+++ src/main/java/com/emanuel/cab/controller/HomeController.java
@@ -1,19 +1,15 @@
 package com.emanuel.cab.controller;
 
 import com.emanuel.cab.dto.UserDto;
-import com.emanuel.cab.model.Userr;
 import com.emanuel.cab.service.IUserService;
-import jakarta.validation.Valid;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class HomeController {
 
     private final IUserService IUserService;
@@ -22,41 +18,11 @@ public class HomeController {
         this.IUserService = IUserService;
     }
 
-    @GetMapping("home")
-    public String home() {
-        return "home";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-
-    @GetMapping("register")
-    public String showRegistrationForm(Model model) throws Exception {
-        UserDto userDto = new UserDto();
-        model.addAttribute("userr", userDto);
-
-        return "register";
-    }
-
-    @PostMapping("/register/save")
-    public String registration(@ModelAttribute("userr") @Valid UserDto userDto,
-                               BindingResult result,
-                               Model model) {
-        Userr existingUserr = IUserService.findUserByUsername(userDto.getUsername());
-
-        if (existingUserr != null) {
-            result.rejectValue("username", "There is already an account registered with that username");
-        }
-
-        if (result.hasErrors()) {
-            model.addAttribute("userr", userDto);
-            return "/register";
-        }
-
-        IUserService.saveUser(userDto);
-        return "redirect:/register?success";
+    @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        return modelAndView;
     }
 
     @GetMapping("/users")
