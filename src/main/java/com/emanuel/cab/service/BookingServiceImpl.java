3c4144c2cb2bce8b5278aa8f4eb6df75f967bf74
emibotea1 <Emanuel.Botea@rinf.tech>
2023-02-08 09:43:21 +0200

    modified booking page and classes

--- src/main/java/com/emanuel/cab/service/BookingServiceImpl.java
+++ src/main/java/com/emanuel/cab/service/BookingServiceImpl.java
@@ -2,7 +2,9 @@ package com.emanuel.cab.service;
 
 import com.emanuel.cab.dto.BookingDto;
 import com.emanuel.cab.model.Booking;
+import com.emanuel.cab.model.Driver;
 import com.emanuel.cab.repository.BookingRepository;
+import com.emanuel.cab.repository.DriverRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,19 +15,19 @@ public class BookingServiceImpl implements IBookingService {
 
     private final BookingRepository bookingRepository;
     private final IUserService userService;
+    private final DriverRepository driverRepository;
 
-    public BookingServiceImpl(BookingRepository bookingRepository, IUserService userService) {
+    public BookingServiceImpl(BookingRepository bookingRepository, IUserService userService,
+                              DriverRepository driverRepository) {
         this.bookingRepository = bookingRepository;
         this.userService = userService;
+        this.driverRepository = driverRepository;
     }
 
-    /*
-    booking.setUsers() -> vreau sa imi seteze id-ul corespunzator user-ului care s-a conectat la aplicatie
-     */
     @Override
     public Booking saveBooking(BookingDto bookingDto) {
         Booking booking = new Booking();
-        //Userr userr = new Userr();
+
         booking.setPickUpLocation(bookingDto.getPickUpLocation());
         booking.setDropOffPoint(bookingDto.getDropOffPoint());
         booking.setCreatedAt(System.currentTimeMillis());
@@ -35,6 +37,12 @@ public class BookingServiceImpl implements IBookingService {
 
         return booking;
     }
+
+    @Override
+    public List<Driver> findAllAvailableDrivers() {
+        return driverRepository.findAllDriversByAvailability();
+    }
+
 
     @Override
     public List<BookingDto> findAllBooking() {
